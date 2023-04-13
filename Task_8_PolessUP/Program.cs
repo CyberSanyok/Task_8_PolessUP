ce445d322d212db26819c3f3290f4213b2e45c19
CyberSanyok <sashad92175@gmail.com>
2023-04-13 10:23:17 +0300

    Добавьте файлы проекта.

---
+++ Task_8_PolessUP/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string s = "";
+        int[] rgb = { -88, 1, 999 };
+        for (int i = 0; i < rgb.Length; i++)
+        {
+            if (rgb[i] >255) rgb[i] = 255;
+            if (rgb[i] < 0) rgb[i] = 0;
+            if (rgb[i].ToString().Length == 1) s += "0" + Convert.ToString(+rgb[i]);
+            else
+            s += Convert.ToString(rgb[i], 16);
+        }
+        Console.WriteLine(s);
+    }
+}
